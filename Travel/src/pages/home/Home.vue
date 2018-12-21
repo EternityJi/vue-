71c4efaf125d3f8781392e32bf8ed912c105e079
姬艳迪 <1002332733@qq.com>
2018-12-22 04:31:34 +0800

    首页轮播图部分完成

--- Travel/src/pages/home/Home.vue
+++ Travel/src/pages/home/Home.vue
@@ -2,15 +2,18 @@
   <div>
     <!-- home-header ==> HomeHeader  vue会自动拆分 -->
     <home-header></home-header>
+    <home-swiper></home-swiper>
   </div>
 </template>
 <script>
 // 局部组件
 import HomeHeader from './components/Header.vue'
+import HomeSwiper from './components/Swiper.vue'
 export default {
   name: 'Home',
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
   }
 }
 </script>
---
+++ Travel/src/pages/home/components/Swiper.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id"><img class="swiper-img"
+             src="item.imgUrl"></swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"
+           slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/20181214_cpm_10281.jpg'
+      }, {
+        id: '0002',
+        imgUrl: '//img1.qunarzz.com/vc/a4/29/dd/4a0734b77f675da2a8ca4a9879.jpg'
+      }, {
+        id: '0003',
+        imgUrl: '//img1.qunarzz.com/vc/f5/10/9c/b647d3ee724a720ce0fa6101f0.jpg'
+      }]
+
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+.wrapper >>>.swiper-pagination-bullet-active {
+  background: #fff;
+}
+
+.wrapper {
+  width: 100%;
+  height: 0;
+  overflow: hidden;
+  padding-bottom: 31.25%;
+  background: #eee;
+}
+
+.swiper-img {
+  width: 100%;
+  height: 194px;
+}
+</style>
