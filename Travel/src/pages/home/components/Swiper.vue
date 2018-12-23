c8bb5974c16479680f036fb6c3d8c982a9330817
姬艳迪 <1002332733@qq.com>
2018-12-23 17:40:47 +0800

    首页轮播图完成

--- Travel/src/pages/home/components/Swiper.vue
+++ Travel/src/pages/home/components/Swiper.vue
@@ -1,54 +1,41 @@
 <template>
-  <div class="wrapper">
+  <div class="wraper">
     <swiper :options="swiperOption">
       <!-- slides -->
-      <swiper-slide v-for="item of swiperList" :key="item.id"><img class="swiper-img"
-             src="item.imgUrl"></swiper-slide>
+      <swiper-slide><img class="Swiperimg"
+             src="//img1.qunarzz.com/vc/f5/10/9c/b647d3ee724a720ce0fa6101f0.jpg"></swiper-slide>
+      <swiper-slide><img class="Swiperimg"
+             src="//img1.qunarzz.com/vc/30/e6/6c/7236d07f50947d523bcdeadae8.jpg"></swiper-slide>
+      <swiper-slide><img src=""></swiper-slide>
       <!-- Optional controls -->
       <div class="swiper-pagination"
            slot="pagination"></div>
     </swiper>
   </div>
 </template>
+
 <script>
 export default {
-  name: 'HomeSwiper',
   data () {
     return {
       swiperOption: {
-        pagination: '.swiper-pagination',
-        loop: true
-      },
-      swiperList: [{
-        id: '0001',
-        imgUrl: 'https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/20181214_cpm_10281.jpg'
-      }, {
-        id: '0002',
-        imgUrl: '//img1.qunarzz.com/vc/a4/29/dd/4a0734b77f675da2a8ca4a9879.jpg'
-      }, {
-        id: '0003',
-        imgUrl: '//img1.qunarzz.com/vc/f5/10/9c/b647d3ee724a720ce0fa6101f0.jpg'
-      }]
-
+        pagination: '.swiper-pagination'
+      }
     }
   }
 }
 </script>
 <style lang="stylus" scoped>
-.wrapper >>>.swiper-pagination-bullet-active {
-  background: #fff;
+.wraper >>> .swiper-pagination-bullet-active {
+  background-color: red !important;
+}
+.Swiperimg {
+  width: 100%;
+  height: 200px;
 }
 
-.wrapper {
-  width: 100%;
-  height: 0;
+.wraper {
   overflow: hidden;
-  padding-bottom: 31.25%;
-  background: #eee;
-}
-
-.swiper-img {
-  width: 100%;
-  height: 194px;
+  background: gray;
 }
 </style>
