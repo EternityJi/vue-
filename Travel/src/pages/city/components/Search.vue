e7aea6e9fb76cd3a269adf61d35dc85bc323a4a4
姬艳迪 <1002332733@qq.com>
2018-12-25 02:42:48 +0800

    修复bug

---
+++ Travel/src/pages/city/components/Search.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="search">
+    <input class="search_input"
+           type="text"
+           placeholder="输入城市名字或者拼音">
+  </div>
+</template>
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+
+.search {
+  height: 0.72rem;
+  background: $bgcolor;
+  padding: 0 0.1rem;
+
+  .search_input {
+    box-sizing: border-box;
+    height: 0.62rem;
+    line-height: 0.62rem;
+    padding: 0 0.1rem;
+    width: 100%;
+    text-align: center;
+    border-radius: 0.06rem;
+    color: #666;
+  }
+}
+</style>
