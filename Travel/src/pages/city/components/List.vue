ef9deb7de7f9652e883212387bb133cc063d6462
姬艳迪 <1002332733@qq.com>
2018-12-26 18:03:08 +0800

    city-list区域滚动部分完成

--- Travel/src/pages/city/components/List.vue
+++ Travel/src/pages/city/components/List.vue
@@ -1,206 +1,215 @@
 <template>
-  <div class="list">
-    <div class="area">
-      <div class="title border-topbottom">当前城市</div>
-      <div class="button-list">
-        <div class="button-wrapper">
-          <div class="button">
-            北京
-          </div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">
-            北京
-          </div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">
-            北京
-          </div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">
-            北京
-          </div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">
-            北京
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">热门城市</div>
-      <div class="button-list">
-        <div class="button-wrapper">
-          <div class="button">
-            北京
-          </div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">
-            北京
-          </div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">
-            北京
-          </div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">
-            北京
-          </div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">
-            北京
-          </div>
-        </div>
-      </div>
-    </div>
 
-    <div class="area">
-      <div class="title border-topbottom">A</div>
-      <div class="item-list">
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
+  <div class="list"
+       ref="wrapper">
+    <div>
+      <div class="area">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">
+              北京
+            </div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">
+              北京
+            </div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">
+              北京
+            </div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">
+              北京
+            </div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">
+              北京
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">A</div>
-      <div class="item-list">
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">
+              北京
+            </div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">
+              北京
+            </div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">
+              北京
+            </div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">
+              北京
+            </div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">
+              北京
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">A</div>
-      <div class="item-list">
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
+
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+        </div>
       </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">A</div>
-      <div class="item-list">
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+        </div>
       </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">A</div>
-      <div class="item-list">
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+        </div>
       </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">A</div>
-      <div class="item-list">
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+        </div>
       </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">A</div>
-      <div class="item-list">
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+        </div>
       </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">A</div>
-      <div class="item-list">
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+        </div>
       </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">A</div>
-      <div class="item-list">
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
-        <div class="item"
-             border-bottom>aaa</div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+          <div class="item"
+               border-bottom>aaa</div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 <script>
+import Bscroll from 'better-scroll'
 export default {
-  name: 'CityList'
+  name: 'CityList',
+  mounted () {
+    // console.log(this.$refs)
+    this.scroll = new Bscroll(this.$refs.wrapper)
+  }
 }
 </script>
 <style lang="stylus" scoped>
